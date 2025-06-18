12e0729ba770f9b1329a0ef20734a041c22b178a
shubhamrustagi <rustagishubham2000@gmail.com>
2025-06-18 23:38:37 +0530

    added scrollable

--- frontend/src/components/ChartComponents/LiveTable.jsx
+++ frontend/src/components/ChartComponents/LiveTable.jsx
@@ -10,27 +10,30 @@ export default function LiveTable() {
     ]
     return (
         <div className="bg-purple-400 col-span-2 flex flex-col items-center justify-center">
-            <h2>Live Vote Table</h2>
-            <table className="w-full border ">
-                <thead>
-                    <tr>
-                        <th>Name</th>
-                        <th>Vote</th>
-                        <th>Time of Submission</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {
-                        dummyVotes.map((vote) => (
-                            <tr className="border">
-                                <td>{vote.name}</td>
-                                <td>{vote.vote}</td>
-                                <td>{vote.timeOfSubmission}</td>
-                            </tr>
-                        ))
-                    }
-                </tbody>
-            </table>
+            <h2 className="text-lg font-semibold mb-2 text-center text-gray-700">Live Vote Table</h2>
+            <div className="w-full max-h-64 overflow-y-auto rounded">
+                <table className="w-full text-sm text-center border ">
+                    <thead className="bg-gray-200 text-gray-700 sticky top-0 z-10">
+                        <tr>
+                            <th className="px-4 py-2">Name</th>
+                            <th className="px-4 py-2">Vote</th>
+                            <th className="px-4 py-2">Time of Submission</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            dummyVotes.map((vote) => (
+                                <tr className="border-t even:bg-white odd:bg-gray-500">
+                                    <td className="px-4 py-2">{vote.name}</td>
+                                    <td className="px-4 py-2">{vote.vote}</td>
+                                    <td className="px-4 py-2">{vote.timeOfSubmission}</td>
+                                </tr>
+                            ))
+                        }
+                    </tbody>
+                </table>
+            </div>
+
         </div>
     )
 }
