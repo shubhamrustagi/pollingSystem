1b1280648547947945409b314a7b9eddc5a5ae26
shubhamrustagi <rustagishubham2000@gmail.com>
2025-06-18 19:24:49 +0530

    minor changes on left section, right section

--- frontend/src/components/LeftSection.jsx
+++ frontend/src/components/LeftSection.jsx
@@ -1,33 +1,41 @@
+import { useState } from "react";
+
 export default function LeftSection() {
 
+    const [name, setName] = useState("");
+    const [voteValue, setVoteValue] = useState(null);
 
 
     function handleSubmit(e) {
         e.preventDefault()
-        const formData = new FormData(e.target);
-        const name = formData.get('name');
-        const vote = formData.get('vote');
+
         console.log("Username: ", name);
-        console.log("Vote: ", vote);
+        console.log("Vote: ", voteValue);
     }
     return (
         <>
             <div className='w-1/2 bg-amber-200 flex flex-col items-center justify-center'>
                 <form className='flex flex-col' onSubmit={handleSubmit}>
                     <label className="text-lg font-medium text-gray-700">Please enter your name</label>
-                    <input type="text" placeholder="Username" name="name" required className="border border-gray-300 px-4 py-2 rounded focus:outline-none focus:ring-2 focus:ring-blue-400 bg-green-400"></input>
+                    <input
+                        type="text"
+                        placeholder="Username"
+                        name="name"
+                        required
+                        className="border border-gray-300 px-4 py-2 rounded focus:outline-none focus:ring-2 focus:ring-blue-400 bg-white"
+                        onChange={(e) => setName(e.target.value)} />
                     <label className="text-lg font-medium text-gray-700">Please select your vote</label>
                     <div className="flex gap-6 items-center justify-center">
-                        <label>
-                            <input type="radio" name="vote" value="yes" />
+                        <label className="text-lg font-medium text-gray-700">
+                            <input type="radio" name="vote" value={true} required onChange={(e) => setVoteValue(e.target.value)} />
                             Yes
                         </label>
-                        <label>
-                            <input type="radio" name="vote" value="no" />
+                        <label className="text-lg font-medium text-gray-700">
+                            <input type="radio" name="vote" value={false} onChange={(e) => setVoteValue(e.target.value)} />
                             No
                         </label>
                     </div>
-                    <button className="mt-4 bg-blue-300 text-white py-2 rounded hover:bg-blue-400 transition"> Click to enter your vote</button>
+                    <button className="mt-4 bg-green-300 py-2 rounded hover:bg-green-400 transition text-lg font-medium text-gray-700"> Click to enter your vote</button>
                 </form>
             </div>
         </>
--- frontend/src/components/RightSection.jsx
+++ frontend/src/components/RightSection.jsx
@@ -1,8 +1,18 @@
 export default function RightSection() {
     return (
         <>
-            <div className='w-1/2 bg-blue-500'>
-                <h1> this is rightsection</h1>
+            <div className='w-1/2 bg-blue-500 h-screen'>
+                <div className="grid grid-cols-2 gap-4 h-screen p-4">
+                    <div className="bg-purple-400 col-span-2 flex flex-col items-center justify-center">
+                        01
+                    </div>
+                    <div className="bg-red-400 flex flex-col items-center justify-center">
+                        02
+                    </div>
+                    <div className="bg-green-400 flex flex-col items-center justify-center">
+                        03
+                    </div>
+                </div>
             </div>
         </>
     )
